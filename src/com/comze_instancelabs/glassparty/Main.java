a67a1bc5eb77a5e434405ce49ca289fae0631890
instance01 <danielratke97@hotmail.de>
2013-12-04 18:22:39 +0100

    reset bugfix

--- src/com/comze_instancelabs/glassparty/Main.java
+++ src/com/comze_instancelabs/glassparty/Main.java
@@ -1,7 +1,9 @@
 package com.comze_instancelabs.glassparty;
 
+import java.util.HashMap;
 import java.util.Random;
 
+import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.event.EventHandler;
@@ -11,41 +13,37 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin implements Listener {
 
+	HashMap<Location, Byte> toremove = new HashMap<Location, Byte>();
+	
 	Random r = new Random();
 	
 	@Override
     public void onEnable(){
 		getServer().getPluginManager().registerEvents(this, this);
+		
+		// reset timer
+		Bukkit.getServer().getScheduler().scheduleSyncRepeatingTask(this, new Runnable(){
+			@Override
+			public void run() {
+				for(Location l : toremove.keySet()){
+					l.getWorld().getBlockAt(l).setData(toremove.get(l));
+				}
+				toremove.clear();
+			}
+		}, 100, 100); // 5 seconds
 	}
 
     @EventHandler
     public void onmove(PlayerMoveEvent event){ 
         int x = event.getFrom().getBlockX();
-        int fromy = event.getFrom().getBlockY();
-        int y = fromy - 1;
+        int y = event.getFrom().getBlockY() - 1;
         int z = event.getFrom().getBlockZ();
         Location loc = new Location(event.getFrom().getWorld(), x, y, z);
-        if (!loc.getBlock().isEmpty() && !loc.getBlock().isLiquid() && loc.getBlock().getType() == Material.STAINED_GLASS){
-        	/*Location loc_ = new Location(event.getFrom().getWorld(), x + 1, y, z);
-        	Location loc_1 = new Location(event.getFrom().getWorld(), x + 1, y, z + 1);
-        	Location loc_2 = new Location(event.getFrom().getWorld(), x + 1, y, z - 1);
-        	Location loc_3 = new Location(event.getFrom().getWorld(), x - 1, y, z);
-        	Location loc_4 = new Location(event.getFrom().getWorld(), x - 1, y, z + 1);
-        	Location loc_5 = new Location(event.getFrom().getWorld(), x - 1, y, z - 1);
-        	Location loc_6 = new Location(event.getFrom().getWorld(), x, y, z - 1);
-        	Location loc_7 = new Location(event.getFrom().getWorld(), x, y, z + 1);
-        	
-        	loc_.getBlock().setData((byte)0);
-        	loc_1.getBlock().setData((byte)0);
-        	loc_2.getBlock().setData((byte)0);
-        	loc_3.getBlock().setData((byte)0);
-        	loc_4.getBlock().setData((byte)0);
-        	loc_5.getBlock().setData((byte)0);
-        	loc_6.getBlock().setData((byte)0);
-        	loc_7.getBlock().setData((byte)0);*/
-        	
+        if (loc.getBlock().getType() == Material.STAINED_GLASS){
+        	if(!toremove.containsKey(loc)){
+        		toremove.put(loc, loc.getBlock().getData());
+        	}
         	loc.getBlock().setData((byte)r.nextInt(15));
         }
     }
-	
 }
